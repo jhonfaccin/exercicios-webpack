48df2fa463c91119d6058f731184999730d5364c
jhon faccin <jhonfaccin@gmail.com>
2018-06-19 17:52:30 -0300

    exercicio 5

---
+++ ex.5/index.js
@@ -0,0 +1,12 @@
+class Pessoa{
+    constructor(nome){
+        this.nome = nome
+    }
+
+    toString () {
+        return `Pessoa: ${this.nome}`
+    }
+}
+
+const pessoa = new Pessoa('guilerme')
+console.log(pessoa.toString())
